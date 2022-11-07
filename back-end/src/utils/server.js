b2a0395cb50626ef920cbab9916d3ab0efb87f32
Slavyan Hristov <slavqn.hristov@gmail.com>
2022-11-07 13:09:48 +0200

    Basic Express App Setup

--- back-end/src/utils/server.js
+++ back-end/src/utils/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const helmet = require("helmet");
+const cors = require("cors");
 
 const createServer = () => {
   const app = express();
 
-  app.use(express.json());
+  app.use(cors());
   app.use(helmet());
+  app.use(express.json());
   app.use(require("../routes"));
   return app;
 };
